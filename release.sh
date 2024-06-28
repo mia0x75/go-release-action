f24e95bb84ce6e991e57259f13d48e54cc71a6e4
mia0x75 <mia0x75@users.noreply.github.com>
2024-06-28 23:28:16 +0800

    Update release.sh

--- release.sh
+++ release.sh
@@ -39,10 +39,10 @@ if [ ! -z "${INPUT_RELEASE_REPO}" ]; then
 fi
 
 # prompt error if non-supported event
-if egrep -q 'release|push|pull_request|workflow_dispatch|workflow_run|schedule' <<<"${GITHUB_EVENT_NAME}"; then
+if egrep -q 'release|push|pull_request|workflow_dispatch|workflow_run|schedule|repository_dispatch' <<<"${GITHUB_EVENT_NAME}"; then
   echo "Event: ${GITHUB_EVENT_NAME}"
 else
-  echo -e "Unsupport event: ${GITHUB_EVENT_NAME}! \nSupport: release | push | pull_request | workflow_dispatch | workflow_run | schedule"
+  echo -e "Unsupport event: ${GITHUB_EVENT_NAME}! \nSupport: release | push | pull_request | workflow_dispatch | workflow_run | schedule | repository_dispatch"
   exit 1
 fi
 
